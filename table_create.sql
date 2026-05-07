dea1d3fa5c4acace1cdd2493c374d8e2e430ffbb
Leila <ld96179p@pace.edu>
2026-05-07 13:29:45 -0400

    auto incrementing city id & created a temp fly data table

--- table_create.sql
+++ table_create.sql
@@ -31,3 +31,22 @@ CREATE TABLE Flight (
     flight_count INT,
     FOREIGN KEY (route_id) REFERENCES Route(route_id) 
 );
+
+ALTER TABLE city
+MODIFY city_id INT AUTO_INCREMENT;
+
+CREATE TABLE temp_flight_data (
+    origin_airport CHAR(3),
+    destination_airport CHAR(3),
+    origin_city VARCHAR(100),
+    origin_state_code CHAR(2),
+    destination_city VARCHAR(100),
+    destination_state_code CHAR(2),
+    passengers INT,
+    seats INT,
+    flight_count INT,
+    distance INT,
+    fly_date INT, -- YYYYM format + dataset description said INT --
+    origin_population INT,
+    destination_population INT
+);
